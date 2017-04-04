ba7828edb7d8fc6a65a565ca615b298434b299bd
nimby <nimbyest@gmail.com>
2017-04-04 17:50:14 -0400

    moved all main files into a main folder

--- MainPages/About.php
+++ MainPages/About.php
@@ -3,18 +3,18 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="bootstrap3_defaultTheme/dist/css/bootstrap.css" rel="stylesheet">
-		<link href="AboutCss.css" rel="stylesheet">
+		<link href="../bootstrap3_defaultTheme/dist/css/bootstrap.css" rel="stylesheet">
+		<link href="../css/AboutCss.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 		<title>About Us</title>
 		
 	</head>
 	<body>
-		<?php include 'includes/wikieatsheader.php' ?>
+		<?php include '../includes/wikieatsheader.php' ?>
 
 				<div id = 'description'>
 					<h1>Wiki Eats<br/><small>Git Hungry</small></h1>
-					<img src="images/chef_hat.png" alt="chefHat" width="460" height="345">
+					<img src="../images/chef_hat.png" alt="chefHat" width="460" height="345">
 					<p><b>
 						It's like Wikipedia, but for recipes. Where all of our recipes are submitted by users like you!
 						Users can also rate their favorite recipes, browse recipes by their favorite categories, and browse recipes of their favorite users.</b></p>
@@ -25,7 +25,7 @@
 						<h2>Aploinar Ortega</h2>
 						<div class = "profile">
 							<p class = "authorsnotes"> "I don't know about business, but recipe #85 is best."</p>
-							<img id="images" src="images/apple.png">
+							<img id="images" src="../images/apple.png">
 						</div>
 					</div>
 						<hr/>
@@ -33,7 +33,7 @@
 					<div class="col-md-12">
 						<h2>Aaron Zhang</h2>
 						<div class = "profile">
-							<img id = "images" src="images/yang.png">
+							<img id = "images" src="../images/yang.png">
 								
 							<p class = "authorsnotes"> "Truly the best thing in the buisness since sliced bread, and we probably have a recipe for that."</p>
 						</div>
